7a4e0e482d72aec41007080e001bdea9b9c4a9f8
thetana <zxsa135@naver.com>
2026-02-03 18:37:00 +0900

    chore: 프로젝트 생성 최초 커밋

---
+++ src/main/java/com/calendarproject/CalendarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.calendarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calendarproject/CalendarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
