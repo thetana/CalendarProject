4a699c67ac02028d5d9a1b4ecdcc5b4a23a1e136
thetana <zxsa135@naver.com>
2026-02-04 14:08:20 +0900

    feat: 일정 서비스 추가

---
+++ src/main/java/com/calendarproject/service/ScheduleService.java
@@ -0,0 +1,13 @@
+package com.calendarproject.service;
+
+import com.calendarproject.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+
+}
