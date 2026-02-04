6d75abf99afb9daaeadcf9c5892371dafd8353cc
thetana <zxsa135@naver.com>
2026-02-04 14:04:33 +0900

    feat: 일정 엔티티 추가

---
+++ src/main/java/com/calendarproject/entity/Schedule.java
@@ -0,0 +1,36 @@
+package com.calendarproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30, nullable = false)
+    private String title;
+    @Column(length = 200, nullable = false)
+    private String details;
+    @Column(length = 30, nullable = false)
+    private String writer;
+    @Column(nullable = false)
+    private String password;
+
+    public Schedule(String title, String details, String writer, String password) {
+        this.title = title;
+        this.details = details;
+        this.writer = writer;
+        this.password = password;
+    }
+
+    public void update(String title, String writer) {
+        this.title = title;
+        this.writer = writer;
+    }
+}
