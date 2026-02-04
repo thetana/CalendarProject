c63be2fbceee8e79a57a4ab80d5b9c3703708822
thetana <zxsa135@naver.com>
2026-02-04 14:08:52 +0900

    feat: 일정 서비스에 일정 생성 기능 추가

--- src/main/java/com/calendarproject/service/ScheduleService.java
+++ src/main/java/com/calendarproject/service/ScheduleService.java
@@ -1,13 +1,29 @@
 package com.calendarproject.service;
 
+import com.calendarproject.dto.*;
+import com.calendarproject.entity.Schedule;
 import com.calendarproject.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
-
+    @Transactional
+    public CreateScheduleResponse save(CreateScheduleRequest request) {
+        Schedule schedule = new Schedule(request.title(),
+                request.details(),
+                request.writer(),
+                request.password());
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new CreateScheduleResponse(savedSchedule.getId(),
+                savedSchedule.getTitle(),
+                savedSchedule.getDetails(),
+                savedSchedule.getWriter(),
+                savedSchedule.getCreatedAt(),
+                savedSchedule.getModifiedAt());
+    }
 }
