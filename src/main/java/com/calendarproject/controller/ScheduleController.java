41ad5dde5dabdab35ba77603acb8887644bf14ef
thetana <zxsa135@naver.com>
2026-02-04 14:45:52 +0900

    feat: 일정 컨트롤러에 전체 일정 조회 API 추가

--- src/main/java/com/calendarproject/controller/ScheduleController.java
+++ src/main/java/com/calendarproject/controller/ScheduleController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ScheduleController {
@@ -16,5 +18,11 @@ public class ScheduleController {
     public ResponseEntity<?> create(@RequestBody CreateScheduleRequest request) {
         CreateScheduleResponse result = scheduleService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<GetSchedulesResponse>> getAll(@RequestParam(required = false) String title) {
+        List<GetSchedulesResponse> result = scheduleService.getAll(title);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
