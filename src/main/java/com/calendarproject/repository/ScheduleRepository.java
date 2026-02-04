440a33c0afb257c1bddc3fb89b706bb6717ac745
thetana <zxsa135@naver.com>
2026-02-04 14:39:08 +0900

    feat: 일정 레포 타이틀 검색 및 수정일시 메소드 추가

--- src/main/java/com/calendarproject/repository/ScheduleRepository.java
+++ src/main/java/com/calendarproject/repository/ScheduleRepository.java
@@ -3,6 +3,10 @@ package com.calendarproject.repository;
 import com.calendarproject.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByTitleOrderByModifiedAtDesc(String title);
+    List<Schedule> findAllByOrderByModifiedAtDesc();
 
 }
